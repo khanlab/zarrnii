1d589041cd04edfe73ab3116fe5c80cc9bfc1002
Ali Khan <alik@robarts.ca>
2024-03-10 10:34:03 -0400

    more refactoring and features

--- ome_zarr_neuro/transform.py
+++ ome_zarr_neuro/transform.py
@@ -60,7 +60,7 @@ class TransformSpec:
 
     @classmethod
     def vox2ras_from_image(cls, path:str,level=0):
-        img_type = ImageSpec.check_img_type(path)
+        img_type = DaskImage.check_img_type(path)
 
         if img_type is ImageType.OME_ZARR:
             return cls(TransformType.AFFINE_RAS,affine=cls.get_vox2ras_zarr(path,level))
@@ -72,7 +72,7 @@ class TransformSpec:
 
     @classmethod
     def ras2vox_from_image(cls, path:str,level=0):
-        img_type = ImageSpec.check_img_type(path)
+        img_type = DaskImage.check_img_type(path)
         if img_type is ImageType.OME_ZARR:
             return cls(TransformType.AFFINE_RAS,affine=cls.get_ras2vox_zarr(path,level))
         elif img_type is ImageType.NIFTI:
@@ -148,13 +148,13 @@ class TransformSpec:
             return vecs + disp_vecs
 
 @define
-class ImageSpec:
+class DaskImage:
     darr: da.Array
     ras2vox: TransformSpec = None
     vox2ras: TransformSpec = None
 
     @classmethod
-    def ref_from_path(cls,path,level,channels,chunks):
+    def from_path_as_ref(cls,path,level=0,channels=[0],chunks='auto'):
         """ ref image we dont need actual intensities, just the shape and affine"""
         img_type = cls.check_img_type(path)
         if img_type is ImageType.OME_ZARR:
@@ -179,7 +179,7 @@ class ImageSpec:
        
 
     @classmethod
-    def img_from_path(cls, path,level,channels,chunks):
+    def from_path(cls, path,level=0,channels=[0],chunks='auto'):
         """ returns a dask array whether a nifti or ome_zarr is provided """
         img_type = cls.check_img_type(path)
         if img_type is ImageType.OME_ZARR:
@@ -213,58 +213,123 @@ class ImageSpec:
             return ImageType.UNKNOWN
        
 
+    def apply_transform(self,
+                        *tfm_specs,
+                        ref_dimg):
+        """ return DaskImage applying transform to floating image.
+            this is a lazy function, doesn't do any work until you compute() 
+            on the returned dask array.
+        """
+
+        #transform specs already has the transformations to apply, just need the conversion to/from vox/ras at start and end
+        transforms = []
+        transforms.append(ref_dimg.vox2ras)
+        for tfm in tfm_specs:
+            transforms.append(tfm)
+        transforms.append(self.ras2vox)
+
+
+        #out image in space of ref
+        interp_dimg=ref_dimg
+
+        #perform interpolation on each block in parallel
+        interp_dimg.darr=da.map_blocks(interp_by_block,
+                            ref_dimg.darr, dtype=np.float32,
+                            transform_specs=transforms,
+                            flo_dimg=self)
+
+        return interp_dimg
+                        
+    def get_bounded_subregion(self,points: np.array):
+
+        """ 
+        Uses the extent of points, along with the shape of the dask array,
+        to return a subregion of the dask array that contains the points,
+        along with the grid points corresponding to the indices from the 
+        uncropped dask array (to be used for interpolation).
+
+        if the points go outside the domain of the dask array, then it is
+        capped off with floor or ceil.
+
+        points are Nx3 or Nx4 coordinates (with/without homog coord)
+        darr is the floating image dask array
+
+        We use compute() on the floating dask array to immediately get it
+        since dask doesn't support nd fancy indexing yet that interpn seems to use 
+        """ 
+        pad=1
+        min_extent=np.floor(points.min(axis=1)[:3]-pad).astype('int') 
+        max_extent=np.ceil(points.max(axis=1)[:3]+pad).astype('int')
+
+        min_extent=np.maximum(min_extent,np.zeros(min_extent.shape))
+        max_extent=np.minimum(max_extent,(self.darr.shape[-3],self.darr.shape[-2],self.darr.shape[-1]))
+
+
+        subvol = self.darr[:,min_extent[0]:max_extent[0],
+                              min_extent[1]:max_extent[1],
+                              min_extent[2]:max_extent[2]].compute()
+
+
+         # along with grid points for interpolation
+        grid_points = (np.arange(min_extent[0],max_extent[0]),
+                       np.arange(min_extent[1],max_extent[1]),
+                       np.arange(min_extent[2],max_extent[2]))
+
+        return (grid_points,subvol)
+        
+
+
+
+
+    def to_nifti(self,filename,**kwargs):
+        out_nib = nib.Nifti1Image(self.darr.squeeze().compute(**kwargs),
+                       affine=self.vox2ras.affine)
+        out_nib.to_filename(filename)
+
+    def to_ome_zarr(self,filename,max_layer=4,scaling_method='local_mean'):
+
+
+        coordinate_transformations = []
+        #for each resolution (dataset), we have a list of dicts, transformations to apply.. 
+        #in this case just a single one (scaling by voxel size)
+        voxdim=np.diag(self.vox2ras)[:3]
+
+        for l in range(max_layer+1):
+            
+            coordinate_transformations.append( [{'scale': [1,voxdim[0],(2**l)*voxdim[1],(2**l)*voxdim[2]], #image-pyramids in XY only
+                                                    'type': 'scale'}]) 
+
+
+        axes =  [{'name': 'c', 'type': 'channel'}] + [{'name': ax, 'type': 'space', 'unit': 'micrometer'} for ax in ['z','y','x'] ] 
+
+
+
+        store = zarr.DirectoryStore(filename)
+        root = zarr.group(store,path='/',overwrite=True)
+        scaler = Scaler(max_layer=max_layer,method=scaling_method)
+
+        with ProgressBar():
+            write_image(image=self.darr,
+                                    group=root,
+                                    scaler=scaler,
+                                    coordinate_transformations=coordinate_transformations,
+                                    storage_options={'dimension_separator': '/'},
+                                    axes=axes,
+                               #     metadata={'omero':omero}
+                                        )
 
 
 
 
 
-#this global variable is set by apply_transform, then used in map_blocks with interp_by_blocks
-#global _flo_darr
 
 
 
 
-def get_bounded_subregion(points: np.array,flo_img: ImageSpec ):
-
-    """ 
-    Uses the extent of points, along with the shape of the dask array,
-    to return a subregion of the dask array that contains the points,
-    along with the grid points corresponding to the indices from the 
-    uncropped dask array (to be used for interpolation).
-
-    if the points go outside the domain of the dask array, then it is
-    capped off with floor or ceil.
-
-    points are Nx3 or Nx4 coordinates (with/without homog coord)
-    darr is the floating image dask array
-
-    We use compute() on the floating dask array to immediately get it
-    since dask doesn't support nd fancy indexing yet that interpn seems to use 
-    """ 
-    pad=1
-    min_extent=np.floor(points.min(axis=1)[:3]-pad).astype('int') 
-    max_extent=np.ceil(points.max(axis=1)[:3]+pad).astype('int')
-
-    min_extent=np.maximum(min_extent,np.zeros(min_extent.shape))
-    max_extent=np.minimum(max_extent,(flo_img.darr.shape[-3],flo_img.darr.shape[-2],flo_img.darr.shape[-1]))
-
-
-    subvol = flo_img.darr[:,min_extent[0]:max_extent[0],
-                          min_extent[1]:max_extent[1],
-                          min_extent[2]:max_extent[2]].compute()
-
-
-     # along with grid points for interpolation
-    grid_points = (np.arange(min_extent[0],max_extent[0]),
-                   np.arange(min_extent[1],max_extent[1]),
-                   np.arange(min_extent[2],max_extent[2]))
-
-    return (grid_points,subvol)
-    
 
 def interp_by_block(x,
                     transform_specs:list[TransformSpec],
-                    flo_img:ImageSpec,
+                    flo_dimg:DaskImage,
                     block_info=None,
                     interp_method='linear'
                     ):
@@ -301,7 +366,7 @@ def interp_by_block(x,
 
     
     #find bounding box required for flo vol
-    (grid_points,flo_vol) = get_bounded_subregion(xfm_vecs,flo_img)
+    (grid_points,flo_vol) = flo_dimg.get_bounded_subregion(xfm_vecs)
 
     #then finally interpolate those points on the template dseg volume
     #need to interpolate for each channel
@@ -320,52 +385,6 @@ def interp_by_block(x,
     
 
     return interpolated
-
-
-def apply_transform(flo_img_path:str, #can be ome_zarr or nifti
-                    ref_img_path:str, #can be ome_zarr or nifti
-                    transform_specs:list[str],
-                    channels=[0], #channels to use
-#                    ref_channel=0,#channel to use if ref is ome_zarr
-#                    flo_channel=0,#channel to use if flo is ome_zarr
-                    ref_level=0,#downsampling level to use if ref is ome_zarr
-                    flo_level=0,#downsampling level to use if ref is ome_zarr
-                    ref_chunks='auto', 
-                    flo_chunks='auto') -> da.Array:
-    """ return dask array applying transform to floating image.
-        this is a lazy function, doesn't do any work until you compute() 
-        on the returned dask array.
-
-        note: global _flo_darr variable is needed since I can't pass
-        a dask array as a kwarg to map_blocks -- could get around this
-        by using a class, can do that later...
-    """
-
-    #load dask array for floating image
-#    global _flo_darr
-#    _flo_darr = get_dask_array_from_path(flo_img_path,level=flo_level,channels=channels,chunks=flo_chunks)
-
-    #load reference  (this will be ref space for now)
-#    ref_darr = get_dask_array_from_path(ref_img_path,level=ref_level,channels=channels,chunks=ref_chunks)
-
-
-    flo_img = ImageSpec.img_from_path(flo_img_path,level=flo_level,channels=channels,chunks=flo_chunks)
-    ref_img = ImageSpec.ref_from_path(ref_img_path,level=ref_level,channels=channels,chunks=ref_chunks)
-
-
-    #transform specs already has the transformations to apply, just need the conversion to/from vox/ras at start and end
-    transform_specs.insert(0,ref_img.vox2ras) #prepend
-    transform_specs.append(flo_img.ras2vox)
-
-
-    #perform interpolation on each block in parallel
-    darr_interp=da.map_blocks(interp_by_block,
-                        ref_img.darr, dtype=np.float32,
-                        transform_specs=transform_specs,
-                        flo_img=flo_img)
-
-    return darr_interp
-                    
 
 
 
