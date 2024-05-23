c43d115637ac0a1d4d6444ea7bda5410f486c1c0
Ali Khan <alik@robarts.ca>
2024-05-23 09:55:27 -0400

    use pytest fixtures

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,5 +1,8 @@
-# tests
+import pytest
+from pathlib import Path
 import nibabel as nib
+import tempfile 
+import os
 from ome_zarr_neuro.transform import DaskImage, TransformSpec
 import numpy as np
 from numpy.testing import assert_array_equal, assert_array_almost_equal
@@ -8,19 +11,36 @@ from numpy.testing import assert_array_equal, assert_array_almost_equal
 # TODO: use tempfiles for test images
 
 
-def test_from_nifti_to_nifti():
-    """create a nifti with nibabel, read it with DaskImage, then write it back as a nifti.
-    ensure data, affine and header do not change."""
-
+@pytest.fixture
+def nifti_nib():
     img_size = (100, 50, 200)
     pix_dims = (0.3, 0.2, 1.5, 1)
 
-    nib.Nifti1Image(
+    nifti_nib = nib.Nifti1Image(
         np.random.rand(*img_size), affine=np.diag(pix_dims)
-    ).to_filename("test.nii")
+    )
+
+    return nifti_nib
+
+
+
+@pytest.fixture
+def cleandir():
+    with tempfile.TemporaryDirectory() as newpath:
+        old_cwd = os.getcwd()
+        os.chdir(newpath)
+        yield
+        os.chdir(old_cwd)
+
+@pytest.mark.usefixtures("cleandir")
+def test_from_nifti_to_nifti(nifti_nib):
+    """create a nifti with nibabel, read it with DaskImage, then write it back as a nifti.
+    ensure data, affine and header do not change."""
+
+    nifti_nib.to_filename("test.nii")
     nib_orig = nib.load("test.nii")
 
-    dimg = DaskImage.from_path("./test.nii")
+    dimg = DaskImage.from_path("test.nii")
     dimg.to_nifti("test_fromdimg.nii")
     nib_dimg = nib.load("test_fromdimg.nii")
 
@@ -29,20 +49,16 @@ def test_from_nifti_to_nifti():
     assert_array_equal(nib_orig.get_fdata(), nib_dimg.get_fdata())
 
 
-def test_from_nifti_to_zarr_to_nifti():
+@pytest.mark.usefixtures("cleandir")
+def test_from_nifti_to_zarr_to_nifti(nifti_nib):
     """create a nifti with nibabel, read it with DaskImage, write it back as zarr,
     then read it again with DaskImage,
     ensure data, affine and header do not change."""
 
-    img_size = (100, 50, 200)
-    pix_dims = (0.3, 0.2, 1.5, 1)
-
-    nib.Nifti1Image(
-        np.random.rand(*img_size), affine=np.diag(pix_dims)
-    ).to_filename("test.nii")
+    nifti_nib.to_filename("test.nii")
     nib_orig = nib.load("test.nii")
 
-    dimg = DaskImage.from_path("./test.nii")
+    dimg = DaskImage.from_path("test.nii")
 
     # now we have dimg with axes_nifti == True
     #  this means, the affine is XYZ vox dims
@@ -60,21 +76,16 @@ def test_from_nifti_to_zarr_to_nifti():
     assert_array_equal(dimg.vox2ras.affine, dimg3.vox2ras.affine)
     assert_array_equal(dimg.darr.compute(), dimg3.darr.compute())
 
-
-def test_from_nifti_to_zarr_to_zarr():
+@pytest.mark.usefixtures("cleandir")
+def test_from_nifti_to_zarr_to_zarr(nifti_nib):
     """create a nifti with nibabel, read it with DaskImage, write it back as zarr,
     then read it again with DaskImage,
     ensure data, affine and header do not change."""
 
-    img_size = (100, 50, 200)
-    pix_dims = (0.3, 0.2, 1.5, 1)
-
-    nib.Nifti1Image(
-        np.random.rand(*img_size), affine=np.diag(pix_dims)
-    ).to_filename("test.nii")
+    nifti_nib.to_filename("test.nii")
     nib_orig = nib.load("test.nii")
 
-    dimg = DaskImage.from_path("./test.nii")
+    dimg = DaskImage.from_path("test.nii")
 
     # now we have dimg with axes_nifti == True
     #  this means, the affine is XYZ vox dims
@@ -89,31 +100,28 @@ def test_from_nifti_to_zarr_to_zarr():
     #  this means, the affine is ZYX vox dims negated
 
     assert dimg2.axes_nifti == False
-    assert_array_equal(np.flip(dimg2.darr.shape[1:], axis=0), img_size)
+    assert_array_equal(np.flip(dimg2.darr.shape[1:], axis=0), nifti_nib.get_fdata().shape)
 
     dimg2.to_ome_zarr("test_fromdimg_tozarr.ome.zarr")
     dimg3 = DaskImage.from_path("test_fromdimg_tozarr.ome.zarr")
 
     assert dimg3.axes_nifti == False
-    assert_array_equal(np.flip(dimg3.darr.shape[1:], axis=0), img_size)
+    assert_array_equal(np.flip(dimg3.darr.shape[1:], axis=0), nifti_nib.get_fdata().shape)
 
     print(f"dimg3 from zarr: {dimg3}")
     assert_array_equal(dimg2.vox2ras.affine, dimg3.vox2ras.affine)
     assert_array_equal(dimg2.darr.compute(), dimg3.darr.compute())
 
 
-def test_affine_transform_identify():
+@pytest.mark.usefixtures("cleandir")
+def test_affine_transform_identify(nifti_nib):
     """tests affine transform from nifti to zarr using identity transformation"""
-    img_size = (100, 50, 200)
-    pix_dims = (0.3, 0.2, 1.5, 1)
 
-    nib.Nifti1Image(
-        np.random.rand(*img_size), affine=np.diag(pix_dims)
-    ).to_filename("test.nii")
+    nifti_nib.to_filename("test.nii")
 
-    flo_dimg = DaskImage.from_path("./test.nii")
+    flo_dimg = DaskImage.from_path("test.nii")
 
-    ref_dimg = DaskImage.from_path("./test.nii")
+    ref_dimg = DaskImage.from_path("test.nii")
 
     interp_dimg = flo_dimg.apply_transform(
         TransformSpec.affine_ras_from_array(np.eye(4)), ref_dimg=ref_dimg
@@ -123,22 +131,17 @@ def test_affine_transform_identify():
         flo_dimg.darr.compute(), interp_dimg.darr.compute()
     )
 
-
-def test_transform_indices_flo_to_ref():
+@pytest.mark.usefixtures("cleandir")
+def test_transform_indices_flo_to_ref(nifti_nib):
     """tests transformation of points from floating to reference space"""
     # first try with just identity, floating as nifti, ref as zarr
 
-    img_size = (100, 50, 200)
-    pix_dims = (0.3, 0.2, 1.5, 1)
+    nifti_nib.to_filename("test.nii")
 
-    nib.Nifti1Image(
-        np.random.rand(*img_size), affine=np.diag(pix_dims)
-    ).to_filename("test.nii")
+    DaskImage.from_path("test.nii").to_ome_zarr("test_flo.ome.zarr")
 
-    DaskImage.from_path("./test.nii").to_ome_zarr("./test_flo.ome.zarr")
-
-    ref_dimg = DaskImage.from_path("./test_flo.ome.zarr")
-    flo_dimg = DaskImage.from_path("./test.nii")
+    ref_dimg = DaskImage.from_path("test_flo.ome.zarr")
+    flo_dimg = DaskImage.from_path("test.nii")
 
     ident = TransformSpec.affine_ras_from_array(np.eye(4))
     flo_indices = np.array((99, 49, 199)).reshape(3, 1)
@@ -158,22 +161,17 @@ def test_transform_indices_flo_to_ref():
     print(flo_to_ref_to_flo_indices)
     assert_array_almost_equal(flo_indices, flo_to_ref_to_flo_indices)
 
-
-def test_transform_indices_ref_to_flo():
+@pytest.mark.usefixtures("cleandir")
+def test_transform_indices_ref_to_flo(nifti_nib):
     """tests transformation of points from reference to floating space"""
     # first try with just identity, ref as nifti, flo as zarr
 
-    img_size = (100, 50, 200)
-    pix_dims = (0.3, 0.2, 1.5, 1)
+    nifti_nib.to_filename("test.nii")
 
-    nib.Nifti1Image(
-        np.random.rand(*img_size), affine=np.diag(pix_dims)
-    ).to_filename("test.nii")
+    DaskImage.from_path("test.nii").to_ome_zarr("test_ref.ome.zarr")
 
-    DaskImage.from_path("./test.nii").to_ome_zarr("./test_ref.ome.zarr")
-
-    ref_dimg = DaskImage.from_path("./test_ref.ome.zarr")
-    flo_dimg = DaskImage.from_path("./test.nii")
+    ref_dimg = DaskImage.from_path("test_ref.ome.zarr")
+    flo_dimg = DaskImage.from_path("test.nii")
 
     ident = TransformSpec.affine_ras_from_array(np.eye(4))
     ref_indices = np.array((99, 49, 199)).reshape(3, 1)
